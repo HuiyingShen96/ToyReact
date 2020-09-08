e88e3f04472c12c2d838efa9d2a723f91ff61de3
沈晖莹 <18218343475@163.com>
2020-09-09 00:01:16 +0800

    简单实现三子棋游戏

--- toy-react.js
+++ toy-react.js
@@ -9,7 +9,11 @@ class ElementWrapper {
     if (name.match(/^on([\s\S]+)$/)) {
       this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value);
     } else {
-      this.root.setAttribute(name, value);
+      if (name === 'className') {
+        this.root.setAttribute('class', value);
+      } else {
+        this.root.setAttribute(name, value);
+      }
     }
   }
 
@@ -57,8 +61,15 @@ export class Component {
   }
 
   rerender() {
-    this._range.deleteContents();
-    this[RENDER_TO_DOM](this._range);
+    const oldRange = this._range;
+
+    const range = document.createRange();
+    range.setStart(oldRange.startContainer, oldRange.startOffset);
+    range.setEnd(oldRange.startContainer, oldRange.startOffset);
+    this[RENDER_TO_DOM](range);
+
+    oldRange.setStart(range.endContainer, range.endOffset);
+    oldRange.deleteContents();
   }
 
   setState(newState) {
@@ -96,6 +107,9 @@ export function createElement(type, attributes, ...children) {
     for (const child of children) {
       if (typeof child === 'string') {
         child = new TextWrapper(child); // 使用 createTextNode() 可以创建文本节点
+      }
+      if (child === null) {
+        continue;
       }
       if ((typeof child === 'object') && (child instanceof Array)) {
         insertChildren(child);
